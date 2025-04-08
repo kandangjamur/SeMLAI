bdef7fbf16470a03efe6f904d362d81734c7b6c4
chhafizadil <chhafizadil@gmail.com>
2025-04-08 22:04:16 +0500

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -3,7 +3,7 @@
 # This file can be left empty or used to import all the necessary components
 # from different modules inside the core directory.
 
-from .analysis import analyze_market
+from .analysis import analyze_all_symbols
 from .indicators import get_rsi, get_macd, get_ema
 from .news_sentiment import fetch_news_impact
 from .whale_detector import check_whale_activity
