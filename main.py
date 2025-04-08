b1ed0ad74751217be2fbfd2c00df4ed5347a97b6
chhafizadil <chhafizadil@gmail.com>
2025-04-08 22:52:47 +0500

    Update main.py

--- main.py
+++ main.py
@@ -52,4 +52,7 @@ def manual_scan():
         return jsonify({"status": "error", "message": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    import os
+port = int(os.environ.get("PORT", 8000))
+app.run(host="0.0.0.0", port=port, debug=True)
+
