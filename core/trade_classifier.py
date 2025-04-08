c3ed3349f0cd306fbf90eb97ede659cf776f2c47
chhafizadil <chhafizadil@gmail.com>
2025-04-08 22:19:24 +0500

    Update trade_classifier.py

--- core/trade_classifier.py
+++ core/trade_classifier.py
@@ -1,4 +1,4 @@
-def classify_trade_type(rsi, macd, ema, whale_activity, news_impact):
+def classify_trade(rsi, macd, ema, whale_activity, news_impact):
     if rsi < 30 and macd[0] > macd[1] and whale_activity:
         return "scalping"
     elif rsi > 70 and macd[0] < macd[1] and news_impact == '🔴 Negative':
