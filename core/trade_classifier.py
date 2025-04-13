6150e093a83e87a7c346d343b5d5f87741b2becf
chhafizadil <chhafizadil@gmail.com>
2025-04-13 21:46:11 +0500

    Delete core directory

--- core/trade_classifier.py
+++
@@ -1,8 +0,0 @@
-def classify_trade(rsi, macd, ema, whale_activity, news_impact):
-    if rsi < 30 and macd[0] > macd[1] and whale_activity:
-        return "scalping"
-    elif rsi > 70 and macd[0] < macd[1] and news_impact == '🔴 Negative':
-        return "normal"
-    elif whale_activity:
-        return "spot"
-    return "normal"
