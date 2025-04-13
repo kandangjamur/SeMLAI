056ab9ba35dbabb79edab187a92c678de7fd07ec
chhafizadil <chhafizadil@gmail.com>
2025-04-13 21:46:03 +0500

    Delete api directory

--- api/routes.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, jsonify
-from core.analysis import generate_signals
-
-app = Flask(__name__)
-
-@app.route('/get_signals', methods=['GET'])
-def get_signals():
-    signals = generate_signals()
-    return jsonify(signals)
