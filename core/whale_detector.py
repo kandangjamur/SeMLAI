c2f6abe0e118801c91087db3c626e58273f720cb
chhafizadil <chhafizadil@gmail.com>
2025-04-21 08:47:05 +0500

    Update whale_detector.py

--- core/whale_detector.py
+++ core/whale_detector.py
@@ -1,9 +1,7 @@
 def whale_check(symbol, exchange):
     try:
-        book = exchange.fetch_order_book(symbol)
-        bid_vol = sum([b[1] for b in book['bids'][:10]])
-        ask_vol = sum([a[1] for a in book['asks'][:10]])
-        imbalance = abs(bid_vol - ask_vol) / max(bid_vol, ask_vol)
-        return imbalance > 0.2
+        ticker = exchange.fetch_ticker(symbol)
+        volume = ticker.get('quoteVolume', 0)
+        return volume > 1000000  # 1M+ USDT volume
     except:
         return False
