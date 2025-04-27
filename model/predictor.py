4f6c04d899aa880c1aafe9cf321e088a3fa3a6cc
chhafizadil <chhafizadil@gmail.com>
2025-04-27 09:59:46 +0500

    Update predictor.py

--- model/predictor.py
+++ model/predictor.py
@@ -1,7 +1,8 @@
 def predict_trend(symbol, ohlcv):
-    closes = [c[4] for c in ohlcv]
-    if closes[-1] > closes[-2] > closes[-3]:
+    closes = [c[4] for c in ohlcv]  # Extracting closing prices
+    if closes[-1] > closes[-2] > closes[-3]:  # Uptrend
         return "LONG"
-    elif closes[-1] < closes[-2] < closes[-3]:
+    elif closes[-1] < closes[-2] < closes[-3]:  # Downtrend
         return "SHORT"
-    return "NEUTRAL"
+    else:
+        return None  # Don't return "NEUTRAL", now returning None for no signal
