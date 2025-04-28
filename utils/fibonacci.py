b3d8bdbafa7fecfe35f959db40b33ec574d1aeba
chhafizadil <chhafizadil@gmail.com>
2025-04-28 10:20:03 +0500

    Update fibonacci.py

--- utils/fibonacci.py
+++ utils/fibonacci.py
@@ -1,11 +1,12 @@
+# utils/fibonacci.py
 def calculate_fibonacci_levels(price, direction="LONG"):
     levels = {}
     if direction == "LONG":
-        levels["tp1"] = price * 1.02
-        levels["tp2"] = price * 1.04
-        levels["tp3"] = price * 1.08
+        levels["tp1"] = price * 1.012
+        levels["tp2"] = price * 1.018
+        levels["tp3"] = price * 1.025
     else:
-        levels["tp1"] = price * 0.98
-        levels["tp2"] = price * 0.96
-        levels["tp3"] = price * 0.92
+        levels["tp1"] = price * 0.988
+        levels["tp2"] = price * 0.982
+        levels["tp3"] = price * 0.975
     return levels
