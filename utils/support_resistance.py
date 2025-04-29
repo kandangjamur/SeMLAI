826da2fa21058830df70ab038956f85245a5d6df
chhafizadil <chhafizadil@gmail.com>
2025-04-29 11:59:01 +0500

    Update support_resistance.py

--- utils/support_resistance.py
+++ utils/support_resistance.py
@@ -1,5 +1,6 @@
+# utils/support_resistance.py
 def detect_sr_levels(df):
     recent = df.tail(20)
-    support = recent['low'].min()
-    resistance = recent['high'].max()
+    support = recent["low"].min()
+    resistance = recent["high"].max()
     return {"support": support, "resistance": resistance}
