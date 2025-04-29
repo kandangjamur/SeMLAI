7fc4207667df315e1d1fc5e50a9e3ba502fb646d
chhafizadil <chhafizadil@gmail.com>
2025-04-29 12:42:04 +0500

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,4 +1,3 @@
-# utils/logger.py
 import os
 import pandas as pd
 from datetime import datetime
@@ -10,28 +9,26 @@ def log_signal_to_csv(signal):
     filename = "logs/signals_log.csv"
     os.makedirs(os.path.dirname(filename), exist_ok=True)
 
-    data = {
-        "timestamp": [signal['timestamp']],
-        "symbol": [signal['symbol']],
-        "price": [signal['price']],
-        "confidence": [signal['confidence']],
-        "tp1": [signal['tp1']],
-        "tp2": [signal['tp2']],
-        "tp3": [signal['tp3']],
-        "sl": [signal['sl']],
-        "type": [signal['trade_type']],
-        "direction": [signal['prediction']],
-        "leverage": [signal['leverage']],
-        "tp1_possibility": [signal.get('tp1_possibility', 0)],
-        "tp2_possibility": [signal.get('tp2_possibility', 0)],
-        "tp3_possibility": [signal.get('tp3_possibility', 0)],
-        "status": ["pending"]
-    }
+    df = pd.DataFrame([{
+        "timestamp": signal['timestamp'],
+        "symbol": signal['symbol'],
+        "price": signal['price'],
+        "confidence": signal['confidence'],
+        "tp1": signal['tp1'],
+        "tp2": signal['tp2'],
+        "tp3": signal['tp3'],
+        "sl": signal['sl'],
+        "type": signal['trade_type'],
+        "direction": signal['prediction'],
+        "leverage": signal['leverage'],
+        "tp1_possibility": signal.get("tp1_possibility", 0),
+        "tp2_possibility": signal.get("tp2_possibility", 0),
+        "tp3_possibility": signal.get("tp3_possibility", 0),
+        "status": "pending"
+    }])
 
     if os.path.exists(filename):
-        df = pd.read_csv(filename)
-        df = pd.concat([df, pd.DataFrame(data)], ignore_index=True)
-    else:
-        df = pd.DataFrame(data)
+        old_df = pd.read_csv(filename)
+        df = pd.concat([old_df, df], ignore_index=True)
 
     df.to_csv(filename, index=False)
