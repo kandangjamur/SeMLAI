81c8ab60d393e2d466b147fec14552a6777a23fb
chhafizadil <chhafizadil@gmail.com>
2025-04-30 22:30:06 +0500

    Update whale_detector.py

--- core/whale_detector.py
+++ core/whale_detector.py
@@ -1,6 +1,6 @@
-def whale_check(symbol, exchange):
+async def whale_check(symbol, exchange):
     try:
-        ticker = exchange.fetch_ticker(symbol)
+        ticker = await exchange.fetch_ticker(symbol)
         volume = ticker.get('quoteVolume', 0)
         return volume > 1000000  # 1M+ USDT volume
     except:
