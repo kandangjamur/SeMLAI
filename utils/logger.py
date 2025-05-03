cd106aa89da35064a0d9000c43607dde6755554e
chhafizadil <chhafizadil@gmail.com>
2025-05-04 02:35:28 +0500

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,8 +1,39 @@
 import logging
+import os
+from logging.handlers import RotatingFileHandler
 
-logger = logging.getLogger("signal_bot")
+LOG_DIR = "logs"
+os.makedirs(LOG_DIR, exist_ok=True)
+
+log_formatter = logging.Formatter(
+    fmt="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S"
+)
+
+log_file = os.path.join(LOG_DIR, "bot.log")
+
+file_handler = RotatingFileHandler(log_file, maxBytes=5 * 1024 * 1024, backupCount=3)
+file_handler.setFormatter(log_formatter)
+file_handler.setLevel(logging.INFO)
+
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(log_formatter)
+console_handler.setLevel(logging.INFO)
+
+logger = logging.getLogger("crypto-signal-bot")
 logger.setLevel(logging.INFO)
-handler = logging.StreamHandler()
-formatter = logging.Formatter("[%(asctime)s] %(levelname)s: %(message)s")
-handler.setFormatter(formatter)
-logger.addHandler(handler)
+logger.addHandler(file_handler)
+logger.addHandler(console_handler)
+logger.propagate = False
+
+def log_info(message: str):
+    logger.info(message)
+
+def log_warning(message: str):
+    logger.warning(message)
+
+def log_error(message: str):
+    logger.error(message)
+
+def log_debug(message: str):
+    logger.debug(message)
