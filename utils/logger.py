7e27bfa3d9e5cab9307418c022f783a4d948eaaa
chhafizadil <chhafizadil@gmail.com>
2025-05-04 02:30:25 +0500

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,26 +1,8 @@
 import logging
-from logging.handlers import RotatingFileHandler
-import os
 
-def setup_logger():
-    log_dir = "logs"
-    os.makedirs(log_dir, exist_ok=True)
-
-    logger = logging.getLogger("crypto-bot")
-    logger.setLevel(logging.INFO)
-
-    if not logger.handlers:
-        # Console Handler
-        console_handler = logging.StreamHandler()
-        console_formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(message)s')
-        console_handler.setFormatter(console_formatter)
-
-        # File Handler with Rotation
-        file_handler = RotatingFileHandler(f"{log_dir}/bot.log", maxBytes=5_000_000, backupCount=5)
-        file_formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(message)s')
-        file_handler.setFormatter(file_formatter)
-
-        logger.addHandler(console_handler)
-        logger.addHandler(file_handler)
-
-    return logger
+logger = logging.getLogger("signal_bot")
+logger.setLevel(logging.INFO)
+handler = logging.StreamHandler()
+formatter = logging.Formatter("[%(asctime)s] %(levelname)s: %(message)s")
+handler.setFormatter(formatter)
+logger.addHandler(handler)
