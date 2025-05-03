e823417ffd81fbb50160dd642ca0cd36ad3bdde2
chhafizadil <chhafizadil@gmail.com>
2025-05-04 01:51:54 +0500

    Update sender.py

--- telebot/sender.py
+++ telebot/sender.py
@@ -2,11 +2,12 @@
 
 import httpx
 
+BOT_TOKEN = "YOUR_BOT_TOKEN"
+CHAT_ID = "YOUR_CHAT_ID"
+
 async def send_telegram_signal(message: str):
-    bot_token = "YOUR_BOT_TOKEN"
-    chat_id = "YOUR_CHAT_ID"
-    url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
-    payload = {"chat_id": chat_id, "text": message}
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    payload = {"chat_id": CHAT_ID, "text": message}
 
     async with httpx.AsyncClient() as client:
         await client.post(url, data=payload)
