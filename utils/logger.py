6755a409bf4e1e303ebefb7d2600c0ef05e97a21
chhafizadil <chhafizadil@gmail.com>
2025-05-03 19:12:37 +0500

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,20 +1,10 @@
 import logging
-import os
 
 def setup_logger(name):
     logger = logging.getLogger(name)
     logger.setLevel(logging.INFO)
-
     if not logger.handlers:
-        os.makedirs("logs", exist_ok=True)
-        file_handler = logging.FileHandler(f"logs/{name}.log")
-        stream_handler = logging.StreamHandler()
-
-        formatter = logging.Formatter('[%(asctime)s] %(message)s', "%Y-%m-%d %H:%M:%S")
-        file_handler.setFormatter(formatter)
-        stream_handler.setFormatter(formatter)
-
-        logger.addHandler(file_handler)
-        logger.addHandler(stream_handler)
-
+        ch = logging.StreamHandler()
+        ch.setFormatter(logging.Formatter("[%(asctime)s] %(message)s"))
+        logger.addHandler(ch)
     return logger
